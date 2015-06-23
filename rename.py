4997a998950b71e4445cde4fe2ce58b9bea36795
slerpy <slerpy@riseup.net>
2015-06-23 04:40:35 -0700

    script to remove numbers and dashes from all files in a directory.

---
+++ rename.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+##############################################################################
+#
+# rename.py
+# --------------------
+# A simple file renaming script
+#
+# @author slerpy, 0x899319D4251502BA
+# @date 22 June 2015
+# @version 0.0.1
+##############################################################################
+
+import os
+
+## which folder contains the files we're going to rename? path goes between " "
+files_location = r"/path/to/folder/with/files/to/rename"
+
+
+def rename_files():
+    file_ls = os.listdir(files_location)    # grab files list
+    os.chdir(files_location)                # change to folder with files in order to start our rename.
+
+
+    for file_name in file_ls:               # for through and remove any numbers or dashes from file names.
+        os.rename(file_name, file_name.translate(None, "0123456789-"))
+
+rename_files()
